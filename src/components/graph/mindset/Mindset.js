93086a63a4c64abdfe149580ac5cfaddf905d7ba
Lokymil <arrestierchristophe@gmail.com>
2020-06-09 16:24:10 +0200

    Make my own barchart

--- src/components/graph/mindset/Mindset.js
+++ src/components/graph/mindset/Mindset.js
@@ -1,51 +1,292 @@
-import React from "react";
-import { ResponsiveBar } from "@nivo/bar";
+import React, { useState } from "react";
 
 const getInitial = () => [
-  { id: "Critical", "number of person": 0 },
-  { id: "Negative", "number of person": 0 },
-  { id: "Neutral", "number of person": 0 },
-  { id: "Positive", "number of person": 0 },
-  { id: "Excellent", "number of person": 0 },
+  { id: "Critical", number: 0, color: "#e91e63" },
+  { id: "Negative", number: 0, color: "#ff6f00" },
+  { id: "Neutral", number: 0, color: "#ffc107" },
+  { id: "Positive", number: 0, color: "#a5d6a7" },
+  { id: "Excellent", number: 0, color: "#4caf50" },
 ];
-const formattedData = (data) =>
+
+const aggregateData = (data) =>
   data.reduce((acc, item) => {
     const index = 2 + parseInt(item.enterprise) + parseInt(item.personnal);
-    acc[index]["number of person"] += 1;
+    acc[index].number += 1;
     return acc;
   }, getInitial());
 
-const Mindset = ({ data }) => (
-  <ResponsiveBar
-    data={formattedData(data)}
-    keys={["number of person"]}
-    colors={{ scheme: "category10" }}
-    margin={{ top: 50, right: 180, bottom: 50, left: 60 }}
-    legends={[
-      {
-        dataFrom: "keys",
-        anchor: "bottom-right",
-        direction: "column",
-        justify: false,
-        translateX: 120,
-        translateY: 0,
-        itemsSpacing: 2,
-        itemWidth: 100,
-        itemHeight: 20,
-        itemDirection: "left-to-right",
-        itemOpacity: 0.85,
-        symbolSize: 20,
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemOpacity: 1,
-            },
-          },
-        ],
-      },
-    ]}
-  />
-);
+const formatData = (data) => {
+  const aggregatedData = aggregateData(data);
+
+  const highestNumber = [...aggregatedData].sort(
+    (a, b) => b.number - a.number
+  )[0].number;
+
+  const ratio = highestNumber / 300;
+  return aggregatedData.map((item) => ({
+    ...item,
+    height: item.number / ratio,
+  }));
+};
+
+const Mindset = ({ data }) => {
+  const [formattedData] = useState(formatData(data));
+
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        width="400"
+        height="400"
+      >
+        <rect width="400" height="400" fill="transparent"></rect>
+        <line
+          stroke="#dddddd"
+          strokeWidth="1"
+          opacity="1"
+          x1="0"
+          x2="400"
+          y1="300"
+          y2="300"
+        ></line>
+        <g transform="translate(0,300)">
+          <g transform="translate(44,0)" style={{ opacity: 1 }}>
+            <line
+              x1="0"
+              x2="0"
+              y1="0"
+              y2="5"
+              style={{ stroke: "rgb(119, 119, 119)", strokeWidth: "1px" }}
+            ></line>
+            <text
+              dominantBaseline="text-before-edge"
+              textAnchor="middle"
+              transform="translate(0,10) rotate(0)"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                fill: "rgb(51, 51, 51)",
+              }}
+            >
+              Critical
+            </text>
+          </g>
+          <g transform="translate(122,0)" style={{ opacity: 1 }}>
+            <line
+              x1="0"
+              x2="0"
+              y1="0"
+              y2="5"
+              style={{ stroke: "rgb(119, 119, 119)", strokeWidth: "1px" }}
+            ></line>
+            <text
+              dominantBaseline="text-before-edge"
+              textAnchor="middle"
+              transform="translate(0,10) rotate(0)"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                fill: "rgb(51, 51, 51)",
+              }}
+            >
+              Negative
+            </text>
+          </g>
+          <g transform="translate(200,0)" style={{ opacity: 1 }}>
+            <line
+              x1="0"
+              x2="0"
+              y1="0"
+              y2="5"
+              style={{ stroke: "rgb(119, 119, 119)", strokeWidth: "1px" }}
+            ></line>
+            <text
+              dominantBaseline="text-before-edge"
+              textAnchor="middle"
+              transform="translate(0,10) rotate(0)"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                fill: "rgb(51, 51, 51)",
+              }}
+            >
+              Neutral
+            </text>
+          </g>
+          <g transform="translate(278,0)" style={{ opacity: 1 }}>
+            <line
+              x1="0"
+              x2="0"
+              y1="0"
+              y2="5"
+              style={{ stroke: "rgb(119, 119, 119)", strokeWidth: "1px" }}
+            ></line>
+            <text
+              dominantBaseline="text-before-edge"
+              textAnchor="middle"
+              transform="translate(0,10) rotate(0)"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                fill: "rgb(51, 51, 51)",
+              }}
+            >
+              Positive
+            </text>
+          </g>
+          <g transform="translate(356,0)" style={{ opacity: 1 }}>
+            <line
+              x1="0"
+              x2="0"
+              y1="0"
+              y2="5"
+              style={{ stroke: "rgb(119, 119, 119)", strokeWidth: "1px" }}
+            ></line>
+            <text
+              dominantBaseline="text-before-edge"
+              textAnchor="middle"
+              transform="translate(0,10) rotate(0)"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                fill: "rgb(51, 51, 51)",
+              }}
+            >
+              Excellent
+            </text>
+          </g>
+        </g>
+        <g>
+          <g transform="translate(9, 0)">
+            <rect
+              width="70"
+              height={formattedData[0].height}
+              transform={`translate(0, ${300 - formattedData[0].height})`}
+              rx="0"
+              ry="0"
+              fill={formattedData[0].color}
+              strokeWidth="0"
+              stroke={formattedData[0].color}
+            ></rect>
+            <text
+              x="35"
+              y="150"
+              textAnchor="middle"
+              dominantBaseline="central"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                pointerEvents: "none",
+              }}
+            >
+              {formattedData[0].number}
+            </text>
+          </g>
+          <g transform="translate(87, 0)">
+            <rect
+              width="70"
+              height={formattedData[1].height}
+              transform={`translate(0, ${300 - formattedData[1].height})`}
+              rx="0"
+              ry="0"
+              fill={formattedData[1].color}
+              strokeWidth="0"
+              stroke={formattedData[1].color}
+            ></rect>
+            <text
+              x="35"
+              y="150"
+              textAnchor="middle"
+              dominantBaseline="central"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                pointerEvents: "none",
+              }}
+            >
+              {formattedData[1].number}
+            </text>
+          </g>
+          <g transform="translate(165, 0)">
+            <rect
+              width="70"
+              height={formattedData[2].height}
+              transform={`translate(0, ${300 - formattedData[2].height})`}
+              rx="0"
+              ry="0"
+              fill={formattedData[2].color}
+              strokeWidth="0"
+              stroke={formattedData[2].color}
+            ></rect>
+            <text
+              x="35"
+              y="150"
+              textAnchor="middle"
+              dominantBaseline="central"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                pointerEvents: "none",
+              }}
+            >
+              {formattedData[2].number}
+            </text>
+          </g>
+          <g transform="translate(243, 0)">
+            <rect
+              width="70"
+              height={formattedData[3].height}
+              transform={`translate(0, ${300 - formattedData[3].height})`}
+              rx="0"
+              ry="0"
+              fill={formattedData[3].color}
+              strokeWidth="0"
+              stroke={formattedData[3].color}
+            ></rect>
+            <text
+              x="35"
+              y="150"
+              textAnchor="middle"
+              dominantBaseline="central"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                pointerEvents: "none",
+              }}
+            >
+              {formattedData[3].number}
+            </text>
+          </g>
+          <g transform="translate(321, 0)">
+            <rect
+              width="70"
+              height={formattedData[4].height}
+              transform={`translate(0, ${300 - formattedData[4].height})`}
+              rx="0"
+              ry="0"
+              fill={formattedData[4].color}
+              strokeWidth="0"
+              stroke={formattedData[4].color}
+            ></rect>
+            <text
+              x="35"
+              y="150"
+              textAnchor="middle"
+              dominantBaseline="central"
+              style={{
+                fontFamily: "sans-serif",
+                fontSize: "11px",
+                pointerEvents: "none",
+              }}
+            >
+              {formattedData[4].number}
+            </text>
+          </g>
+        </g>
+      </svg>
+    </>
+  );
+};
 
 export default Mindset;
