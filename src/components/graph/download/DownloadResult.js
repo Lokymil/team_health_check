b606b4ef62e23285f178bbf9e9e80a38d97bb97e
Lokymil <arrestierchristophe@gmail.com>
2020-06-10 10:06:23 +0200

    Style download button

--- src/components/graph/download/DownloadResult.js
+++ src/components/graph/download/DownloadResult.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { jsonToCsv } from "../../../utils";
+import "./DownloadResult.scss";
 
 const DownloadResult = ({ data }) => {
   const csvData = "data:text/csv;charset=utf-8," + jsonToCsv(data);
 
   return (
-    <a href={encodeURI(csvData)} download={"health_check.csv"}>
+    <a
+      href={encodeURI(csvData)}
+      download={"health_check.csv"}
+      className="download__link button"
+    >
       Get my results
     </a>
   );
